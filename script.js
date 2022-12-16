0830893d852bd7f8e79bb4cfabf3421bb570728b
yPerrot <y.perrot@outlook.fr>
2022-12-16 23:17:03 +0100

    Improve UX

--- script.js
+++ script.js
@@ -2,19 +2,30 @@ const isChrome = navigator.userAgent.indexOf('Chrome') !== -1;
 // Shamefully stolen from https://github.com/htrinter/Open-Multiple-URLs
 const regExpURL = /\b((?:[a-z][\w-]+:(?:\/{1,3}|[a-z0-9%])|www\d{0,3}[.]|[a-z0-9.-]+[.][a-z]{2,4}\/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()[\]{};:'".,<>?«»“”‘’]))/gi
 
-window.onload = (event) => {
+window.onload = () => {
     const doLoadAllPages = JSON.parse(localStorage.getItem('doLoadAllPages'));
 
     if ( !isChrome && doLoadAllPages) {
        document.querySelector("#switch__checkbox").checked = true;
     }
+
+    document.querySelector('#links').focus();
 }
 
 document.querySelector('#switch__checkbox').onclick = function () {
     localStorage.setItem('doLoadAllPages', document.querySelector("#switch__checkbox").checked)
 }
 
-document.querySelector('#open-btn').onclick = function () {
+document.querySelector('#open-btn').onclick = loadPages;
+document.onkeydown = (event) => {
+    if (event.ctrlKey && event.code === 'Enter') {
+        loadPages();
+        window.close();
+    }
+}
+
+
+function loadPages() {
     const text = document.querySelector('#links').value;
     const doLoad = !document.querySelector("#switch__checkbox").checked;
 
